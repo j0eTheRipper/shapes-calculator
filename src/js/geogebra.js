61576743ab7786163bc5ce00621b6ffe400f95c1
youssef <yalaa129@gmail.com>
2020-01-02 00:34:56 +0300

    started the geogebra.js file

--- src/js/geogebra.js
+++ src/js/geogebra.js
@@ -1,48 +1,59 @@
 window.onload = function () {
-    var regularShapes = { 
-        //these are the defults, and can be changed.
-        numberOfSides: 0,
-        apothemLength: 0,
-        sideLength: 0,
-        //calculautor method
-        calculautor: function (isArea) {
-            var result;
-            var theata = Math.tan(360/this.numberOfSides/2 * Math.PI / 180)
-            if (this.numberOfSides === '') {
-                return alert('please enter the number of sides')
-            } else if (this.apothemLength === '' && this.sideLength === '') {
-                return alert('please enter one of the dimentions, either the side or the apothem');
-            } else {
-                if (isArea === true) {
-                    if (this.apothemLength !== '' && this.sideLength === '') {
-                        result = theata * Math.pow(this.apothemLength, 2) * this.numberOfSides;
-                    } else if (this.apothemLength === '' && this.sideLength !== '') {
-                        result = this.numberOfSides * Math.pow(this.sideLength, 2) / 4 / theata;
-                    } else {
-                        result = this.apothemLength * this.sideLength * this.numberOfSides / 2;
-                    }
+    function calculautor (isArea) {
+        var numberOfSides = document.getElementById('numberOfSides').value;
+        var sideLength = document.getElementById('sideLength-reg').value;
+        var apothemLength = document.getElementById('apothemLength').value;
+        var result;
+        var theata = Math.tan(360/numberOfSides/2 * Math.PI / 180)
+        if (numberOfSides === '') {
+            return alert('please enter the number of sides')
+        } else if (apothemLength === '' && sideLength === '') {
+            return alert('please enter one of the dimentions, either the side or the apothem');
+        } else {
+            if (isArea === true) {
+                if (apothemLength !== '' && sideLength === '') {
+                    result = theata * Math.pow(apothemLength, 2) * numberOfSides;
+                } else if (apothemLength === '' && sideLength !== '') {
+                    result = numberOfSides * Math.pow(sideLength, 2) / 4 / theata;
                 } else {
-                    if (this.apothemLength !== '' && this.sideLength === '') {
-                        result = theata * 2 * this.apothemLength * this.numberOfSides
-                    } else if (this.sideLength !== '') {
-                        result = this.sideLength * this.numberOfSides;
-                    }
+                    result = apothemLength * sideLength * numberOfSides / 2;
+                }
+            } else {
+                if (apothemLength !== '' && sideLength === '') {
+                    result = theata * 2 * apothemLength * numberOfSides
+                } else if (sideLength !== '') {
+                    result = sideLength * numberOfSides;
                 }
             }
-            return alert(result);
+        }
+        return alert(result);
+    }
+    function quadCalculator(isArea, base, height, base2) {
+        if (base2 === undefined) {
+            if (isArea === true) {
+                return alert(base * height);
+            } else {
+                return alert(2 * (base + height));
+            }
+        } else {
+            if (isArea === true) {
+                return alert((base + base2) / 2 * height);
+            } else {
+                return alert(base + base2 + height);
+            }
         }
     }
     document.getElementById('calcAreaReg').onclick = function () {
-        regularShapes.numberOfSides = document.getElementById('numberOfSides').value; 
-        regularShapes.sideLength = document.getElementById('sideLength').value;
-        regularShapes.apothemLength = document.getElementById('apothemLength').value;
-        return regularShapes.calculautor(true);
-    };
-    this.document.getElementById('calcPerReg').onclick = function () {
-        regularShapes.numberOfSides = document.getElementById('numberOfSides').value; 
-        regularShapes.sideLength = document.getElementById('sideLength').value;
-        regularShapes.apothemLength = document.getElementById('apothemLength').value;
-        return regularShapes.calculautor(false);
-    };
+        calculautor(true);
+    }
+    document.getElementById('calcPerReg').onclick = function () {
+        calculautor(false);
+    }
+    document.getElementById('calcRectArea').onclick = function () {
+        quadCalculator(true, document.getElementById('base').value, document.getElementById('height').value)
+    }
+    document.getElementById('calcRectPer').onclick = function () {
+        quadCalculator(false, )
+    }
     //Dont code after this curly brace.
 }
