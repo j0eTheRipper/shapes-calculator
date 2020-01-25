f421309fda4a0fdc8a4e9f3e7f83deeffa3edbd5
youssef-js <yalaa129@gmail.com>
2020-01-25 10:46:06 +0300

    added the regShapes object

--- src/js/geogebra.js
+++ src/js/geogebra.js
@@ -1,9 +1,27 @@
 window.onload = function () {
-    
-    function inputMaker(mergePlace, id, placeholder) {
+    var regShapes = {
+        dimintions: document.getElementById('regShapes-dimintions'),
+        inputSetup: document.getElementById('regShapes-inputSetup'),
+        numOfSides: document.getElementById('numOfSides'),
+        side: document.getElementById('regShapes-side'),
+        apothem: document.getElementById('regShapes-apothem'),
+        setup: function () {
+            if (this.inputSetup === '') {
+                if (this.dimintions.value === 'apothem-side') {
+                    mkinput(this.inputSetup, "apothem", 'apothem')
+                }
+            }
+        }
+    }
+    document.getElementById('helloworld').onclick = function () {
+        regShapes.setup();
+    }
+    //console.log(regShapes.inputSetup.)
+    function mkinput(mergePlace, id, placeholder) {
         var newElement = document.createElement('input');
         newElement.setAttribute('id', id);
         newElement.setAttribute('placeholder', placeholder);
+        newElement.setAttribute('type', 'number');
         document.getElementById(mergePlace).appendChild(newElement);
     }
     //Dont code after this curly brace.
