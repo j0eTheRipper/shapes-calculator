d0f3b3e497a2a2ab9521d30bec2b716e568a0329
youssef-js <yalaa129@gmail.com>
2020-01-27 19:33:51 +0300

    renewed the js file + modified some html IDs

--- src/js/geogebra.js
+++ src/js/geogebra.js
@@ -1,22 +1,20 @@
+//@ts-check
 window.onload = function () {
     var regShapes = {
-        dimintions: document.getElementById('regShapes-dimintions'),
-        inputSetup: document.getElementById('regShapes-inputSetup'),
-        numOfSides: document.getElementById('numOfSides'),
-        side: document.getElementById('regShapes-side'),
-        apothem: document.getElementById('regShapes-apothem'),
-        setup: function () {
-            if (this.inputSetup === '') {
-                if (this.dimintions.value === 'apothem-side') {
-                    mkinput(this.inputSetup, "apothem", 'apothem')
-                }
+        dimintions: document.getElementById('regShapes-dimintions'), 
+        side: document.getElementById('regShapes-sideLength'),
+        apothem: document.getElementById('regShapes-apothemLength'), 
+        inputSetup: function () {
+            
+            if (this.dimintions.value === 'apothem-side') {
+                alert('helloWorld')
+                mkinput('regShapes-mergePlace', 'regShapes-apothemLength', 'apothem');
             }
         }
     }
     document.getElementById('helloworld').onclick = function () {
-        regShapes.setup();
+        regShapes.inputSetup();
     }
-    //console.log(regShapes.inputSetup.)
     function mkinput(mergePlace, id, placeholder) {
         var newElement = document.createElement('input');
         newElement.setAttribute('id', id);
